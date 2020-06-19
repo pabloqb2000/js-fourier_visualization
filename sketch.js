811895bc5d2f49685484250d79e31280b22eb795
pabloqb2000 <qb.pablo.2000@gmail.com>
2020-06-20 00:28:43 +0200

    Started coding

--- sketch.js
+++ sketch.js
@@ -1,5 +1,26 @@
-let sld, btn, tggl, cPicker, txtInpt, optBox;
-let circle, rectangle;
+var N = 1600 // number of input samples
+let path;
+let P;
+
+let done=false;
+async function preload() {
+	//let svg = await fetch("https://gist.githubusercontent.com/mbostock/a4fd7a68925d4039c22996cc1d4862ce/raw/d813a42956d311d73fee336e1b5aac899c835883/fourier.svg")
+	let svg = await fetch("./svg/treble-clef.svg")
+	.then(response => response.text())
+	.then(text => (new DOMParser).parseFromString(text, "image/svg+xml"))
+	.then(svg => svg.documentElement);
+
+	viewbox = svg.viewBox.baseVal
+
+	let path2 = svg.querySelector("path")
+    l = path2.getTotalLength()
+    P = Array.from({length: N}, (_, i) => {
+        const {x, y} = path2.getPointAtLength(i / N * l);
+        return [x - viewbox.width / 2, y - viewbox.height / 2];
+    })
+	console.log(P);
+	done=true;
+}
 
 function setup() {
 	textFont("Orbitron");
@@ -7,18 +28,6 @@ function setup() {
 	background(32);
 
 	// Create UI elements
-	sld = new Slider(start=0, end=255, value=32, 0, 0, width/12, height/60, null, "Background");
-	btn = new Button(x=0, y=0, width/12, height/30, "Reset", resetValue);
-	
-	tggl = new ToggleButton(0,0,width/12,height/30,"Discrete", discretice);
-	cPicker = new ColorPicker(0,0, width/12, height/30, null, "Color 1");
-	
-	txtInpt = new TextInput(16, "Enter text", 0,0, height/25);
-	optBox = new OptionsBox(["Option1", "Option2", "Option3"], 20, () => console.log("Option changed"));
-	
-	// Add two draggable elements
-	circle = new DragCircle(createVector(width/2 + 40, height/2 + 40), 20);
-	rectangle = new DragRect(createVector(width/2 - 40, height/2 - 40), 40, 40);
 
 	// Start UI
 	UI.tableWidth = 1;
@@ -28,21 +37,19 @@ function setup() {
 
 function draw() {
 	// Draw UI and draggable elements
-	background(sld.value);
+	background(32);
 	UI.update();
 	UI.draw();
-	Drag.update();
-	Drag.draw();
-
 	
 	translate(13/24*width, height/2);
 	scale(1,-1);
-}
-
-function resetValue() {
-	sld.value = 32;
-}
-
-function discretice() {
-	sld.step = (sld.step == null ? 20 : null);
+	
+	noFill();
+	stroke(230);
+	strokeWeight(2);
+	beginShape();
+	for(let p of P) {
+		vertex(p[0], p[1]);
+	}
+	endShape();
 }
