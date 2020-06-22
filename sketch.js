db48580c033a5e8639f0ca6239b8ca953a7d9613
pabloqb2000 <qb.pablo.2000@gmail.com>
2020-06-22 11:36:10 +0200

    Now using complex numbers!

--- sketch.js
+++ sketch.js
@@ -1,27 +1,18 @@
-var N = 1600 // number of input samples
-let path;
-let P;
-let drawings = ['clef', 'fourier', 'pi', 'treble-clef']
-let ind = 0;
+let N = 1600; // number of input samples
+let path = null, svg, viewbox;
+let drawings = [{name: 'clef', s: 1},
+				{name: 'fourier', s: 1},
+				{name: 'pi', s: 1},
+				{name: 'treble-clef', s: 20}]
+let ind = 2;
 
-let done=false;
 async function preload() {
-	//let svg = await fetch("https://gist.githubusercontent.com/mbostock/a4fd7a68925d4039c22996cc1d4862ce/raw/d813a42956d311d73fee336e1b5aac899c835883/fourier.svg")
-	let svg = await fetch("https://raw.githubusercontent.com/pabloqb2000/js-fourier_visualization/gh-pages/svg/" + drawings[ind] + ".svg")
+	svg = await fetch("https://raw.githubusercontent.com/pabloqb2000/js-fourier_visualization/gh-pages/svg/" + drawings[ind].name + ".svg")
 	.then(response => response.text())
 	.then(text => (new DOMParser).parseFromString(text, "image/svg+xml"))
 	.then(svg => svg.documentElement);
 
-	viewbox = svg.viewBox.baseVal
-
-	let path2 = svg.querySelector("path")
-    l = path2.getTotalLength()
-    P = Array.from({length: N}, (_, i) => {
-        const {x, y} = path2.getPointAtLength(i / N * l);
-        return [x - viewbox.width / 2, y - viewbox.height / 2];
-    })
-	console.log(P);
-	done=true;
+	path = svg.querySelector("path");
 }
 
 function setup() {
@@ -44,18 +35,31 @@ function draw() {
 	UI.draw();
 	
 	translate(13/24*width, height/2);
-	scale(1,-1);
+	scale(1,1);
 	
-	noFill();
-	stroke(230);
-	strokeWeight(2);
-	beginShape();
-	for(let p of P) {
-		vertex(p[0]*20, p[1]*20);
+	if(path != null) {
+		noFill();
+		stroke(230);
+		strokeWeight(2);
+		beginShape();
+		for(let i = 0; i < N; i++) {
+			let z = pathPt(i/N);
+			vertex(z.r, z.i);
+		}
+		endShape();
+		strokeWeight(5);
+		stroke(255, 0, 0);
+		//let i = frameCount % P.length;
+		//point(P[i][0]*drawings[ind].s, P[i][1]*drawings[ind].s);
 	}
-	endShape();
-	strokeWeight(5);
-	stroke(255, 0, 0);
-	let i = frameCount % P.length;
-	point(P[i][0]*20, P[i][1]*20);
+}
+
+/**
+ * Returns the coordinates of the path
+ * at the corresponding length t
+ */
+function pathPt(t) {
+	viewbox = svg.viewBox.baseVal;
+	const {x, y} = path.getPointAtLength(t * path.getTotalLength());
+	return new Complex([x - viewbox.width/2, y - viewbox.height/2]).mult(drawings[ind].s);
 }
